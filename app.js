d93511f45829e8acad2b40f090e1ae36b8308ce2
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-05-19 11:33:48 +0700

    add cors

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
